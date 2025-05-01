2df9ff52a7a7d6644e524570db1e0ffaec34a9d5
Terence Tao <tao@math.ucla.edu>
2025-05-01 15:47:28 -0700

    create statement class

--- src/estimates.py
+++ src/estimates.py
@@ -1,4 +1,4 @@
-# Bare-bones symbolic expression classes
+# Symbolic expression classes for order of magnitude estimates (where we only care about magnitudes up to a constant factor)
 
 class Expression:
     def __add__(self, other):
@@ -9,6 +9,13 @@ class Expression:
         return Div(self, other)
     def __pow__(self, other):
         return Power(self, other)
+    def __le__(self, other):
+        return Statement(self, '<~', other)
+    def __ge__(self, other):
+        return Statement(self, '>~', other)
+    def __eq__(self, other):
+        return Statement(self, '~', other)
+
     def __str__(self):
         raise NotImplementedError("Must implement __str__ in subclasses")
     def __repr__(self):
@@ -76,9 +83,24 @@ def min(*args):
     # Assuming all args are Expression instances
     return Min(*args)
 
+
+# Overloading comparison operators to create Statement objects
+
+class Statement:
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+    def __str__(self):
+        return f"{self.left} {self.op} {self.right}"
+    def __repr__(self):
+        return str(self)
+
 # Example usage
 N_1 = Variable("N_1")
 N_2 = Variable("N_2")
 N_3 = Variable("N_3")
 expr = (max(N_1, N_2)) * (N_3**3) /N_1
 print("Expression is:", expr)
+S = (expr <= N_2 ** 1)
+print("Statement is:", S)
